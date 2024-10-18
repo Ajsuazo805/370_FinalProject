d06c763aeb853ebc868cb89d77e8ca3f1651e3cd
Ajsuazo805 <Suazoforlife@gmail.com>
2024-10-17 20:35:48 -0700

    Updated player controller to show new jump and added portal script
    deleted and created a new test scene

--- 370_Project/Assets/Scripts/PlayerController.cs
+++ 370_Project/Assets/Scripts/PlayerController.cs
@@ -1,87 +1,126 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+/*
+ * Author: [Suazo, Angel]
+ * Last Updated: [10/17/2024]
+ * [Movement for player script]
+ */
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rigidBody;
-    
-    public float speed = 5f;
+    //This will determine how many lives the player has
+    public int lives = 3;
+
+    //location of where the player respawn to
+    private Vector3 startPos;
+
+    //side to side movement speed
+    public float speed = 10f;
+
+    //jump force added when the player presses space
     public float jumpForce = 8f;
 
-    private bool isGrounded;
-
+    private Rigidbody rigidbody;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        //set the startPos
+        startPos = transform.position;
+        //Set the reference to the players attached rigidbody
+        rigidbody = GetComponent<Rigidbody>();  
     }
+
 
     // Update is called once per frame
     void Update()
     {
         Move();
-        Jump();
+        HandleJumping();
     }
 
+
+    /// <summary>
+    /// lose a live and move the player back to startPos
+    /// </summary>
+    private void Respawn()
+    {
+        lives--;
+        //bring the player back to startPos
+        transform.position = startPos;
+        //check to see if player  has 0 lives
+        if (lives == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+
+    }
 
     private void Move()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Move the player Forward");
-            //W key to go FORWARD
-            transform.position += -transform.forward * speed * Time.deltaTime;
+            transform.position += Vector3.forward * speed * Time.deltaTime;
         }
+        //Left and Right Movement
         if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Move the player Left");
-            //the A key goes LEFT
-            transform.position += transform.right * speed * Time.deltaTime;
+            // Translate the player left by speed using Time.deltaTime.
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("Move the player Back");
-            //the S key goes BACK
-            transform.position += transform.forward * speed * Time.deltaTime;
+            transform.position += Vector3.back * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("Move the player Right");
-            //D key to go RIGHT
-            transform.position += -transform.right * speed * Time.deltaTime;
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
     }
 
-    private void Jump()
+    private void HandleJumping()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1.5f))
+        //Handles jumping 
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (hit.collider.tag == "Floor")
+            RaycastHit hit;
+
+            //if the raycast returns true then an object has been hit and the player is touching the floor
+            //For RayCast(startPosition,RayDirection, output the object hit, maximumDistanceForTheRayCastToFire)
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.5f))
             {
-                isGrounded = true;
+                Debug.Log("Touching the ground");
+                //adds velocity to the player object causing the player to jump up
+                rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             }
             else
             {
-                isGrounded = false;
-                Debug.Log(" not on ground");
+                Debug.Log("Cant Jump, not touching the ground");
             }
         }
-        else
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //if we collide with an enemy trigger, respawn
+        if (other.gameObject.tag == "Enemy")
         {
-            isGrounded = true;
-            Debug.Log("on the ground");
+            Respawn();
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        //if we collide with a portal trigger, reset the start position
+        // and teleport the player to the next area
+        if (other.gameObject.tag == "Portal")
         {
-            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //reset the startPos to the spawnPoint location
+            startPos = other.gameObject.GetComponent<Portal>().spawnPoint.transform.position;
+            //teleport the player to the new startPos
+            transform.position = startPos;
         }
     }
-
 }
---
+++ 370_Project/Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Suazo, Angel]
+ * Last Updated: [10/12/2023]
+ * [Has the spawnPoint game object]
+ */
+
+public class Portal : MonoBehaviour
+{
+    // game object to tell where the spawnPoint is 
+    public GameObject spawnPoint;
+}
