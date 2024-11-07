23cc9e15c37bbb293ad5529c9dd668f78840ce38
Yasmine Arteaga <stepperhold@outlook.com>
2024-11-06 21:23:06 -0800

    PauseControls
    Added a pause menu and resume game.

--- 370_Project/Assets/Scripts/SwitchScenes.cs
+++ 370_Project/Assets/Scripts/SwitchScenes.cs
@@ -5,17 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScenes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ResumeGame()
     {
-        
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
     public void SwitchScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
