aaf137a9850d6810b8ccf140bd1e11fe66dbfdcf
hjgibson <rdohed2@gmail.com>
2024-12-10 09:16:38 -0800

    teleport fixed

--- 370_Project/Assets/Scripts/TeleportV2.cs
+++ 370_Project/Assets/Scripts/TeleportV2.cs
@@ -15,31 +15,50 @@ public class TeleportV2 : MonoBehaviour
 
     public float manaRequired= 1f;
 
+    private bool teleportIsTrue = false;
+
+    private float coolDown = 3f;
+
+    [SerializeField]
+    private float teleportDelay = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-       // playerMana = GetComponent<PlayerController>().playerMana;
+        // playerMana = GetComponent<PlayerController>().playerMana;
+        //StartCoroutine(TeleportTimer());
     }
 
     // Update is called once per frame
     void Update()
     {
         ManaTeleport();
+
+      
     }
     /// <summary>
     /// controls the teleport to correspond with mana 
     /// </summary>
     public void ManaTeleport()
     {
-            if (Input.GetKeyDown(KeyCode.F))
+        
+        if (Input.GetKeyDown(KeyCode.F)&& Time.time > coolDown)
             {
+            
                 if (playerController.playerMana >= manaRequired)
                 {
                 Vector3 teleportPosition = new Vector3(Teleport.transform.position.x, Teleport.transform.position.y, Teleport.transform.position.z);
                     gameObject.transform.position = teleportPosition;
                              Debug.Log("teleport Successful");
                 playerController.playerMana--;
-                }
+                coolDown = Time.time + teleportDelay;
+                Debug.Log("teleport delay");
+                //teleportIsTrue = true;
+              //  StartCoroutine(TeleportTimer());
+                
+               
+                
+            }
                
             else
             {
@@ -49,6 +68,14 @@ public class TeleportV2 : MonoBehaviour
             }
            
            
+    }
+    private IEnumerator TeleportTimer()
+    {
+        
+        Debug.Log("wait to teleport");
+        yield return new WaitForSeconds(3);
+        Debug.Log("you can now teleport");
+       /// teleportIsTrue = false ;
     }
 
 }
